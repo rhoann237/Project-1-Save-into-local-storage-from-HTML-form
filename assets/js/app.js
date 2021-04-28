eef3d9a4574eba697716ceee2e763f55ec67c69c
Rhoann Mae Galicio <rhoannmaegalicio@gmail.com>
2021-04-29 00:12:50 +0800

    Adding new elements nto the DOM - Removing elements from the DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 // Variables
-
+const tweetList = document.getElementById('tweet-list');
 
 
 // Event Listeners
@@ -8,6 +8,9 @@ eventListeners();
 function eventListeners() {
     // Form submission
     document.querySelector("#form").addEventListener('submit', newTweet);
+
+    // Remove tweet from the list
+    tweetList.addEventListener('click', removeTweet);
 }
 
 
@@ -16,5 +19,29 @@ function eventListeners() {
 function newTweet(e) {
     e.preventDefault();
 
-    console.log('Form Submitted');
+    // Read the textarea value
+    const tweet = document.getElementById('tweet').value;
+
+    // Create the remove button
+    const removeBtn = document.createElement('a');
+    removeBtn.classList = 'remove-tweet';
+    removeBtn.textContent = 'X';
+
+    // Create an <li> element
+    const li = document.createElement('li');
+    li.textContent = tweet;
+    
+
+    // Add the remove button to each tweet
+    li.appendChild(removeBtn);
+
+    // Add to the list
+    tweetList.appendChild(li);
+}
+
+// Removes the Tweets from the DOM
+function removeTweet(e) {
+    if(e.target.classList.contains('remove-tweet')) {
+        e.target.parentElement.remove();
+    } 
 }
